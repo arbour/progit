9365a39f3ca6caff0df9cbd37e9cb8f063a25c81
gdzhang <gdzhang@linx-info.com>
2012-08-14 17:50:26 +0800

    zh_cn html progit release 1.0

--- pro2html.sh
+++ pro2html.sh
@@ -27,6 +27,8 @@ echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.or
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<style type="text/css"><!--body{margin:0 20px;font-size:14px;font-family:"Microsoft YaHei","WenQuanYi ZenHei";}pre{margin:1em 0;font-size:13px;background-color:#eee;border:1px solid #ddd;padding:5px;line-height:1.5em;color:#444;overflow:auto;border-radius:3px;}code{background:#eee;}pre,code{font-family:"Deja Vu Sans Mono",Consolas,Monaco,"Courier New",Courier,monospace;}h1{text-align:center;margin-top:30px;color:green}h2{color:#6EA2F8}h3{color:#EE7D2F}.page{border:2px solid #333;background:#333;margin: 30px 0;}img{max-width:600px;display:block;text-align:center;border-radius:5px;}--></style>
 	</head><body>' >> $out
+#make a table 
+echo "<table> <td>" >> $out
 for i in `ls $lang/`
     do 
     list=`find $lang/$i -iname "*.markdown"`
@@ -35,19 +37,32 @@ for i in `ls $lang/`
     #uncomment below for Linux	
     #markdown -v -o 'html4' $list -f $html
     #for windows
-    php ./markdown.php $list $html
+    php htmlscripts/markdown.php $list $html
     cat $html >> $out
     rm $html
     echo '<div class="page"></div>' >> $out
     echo "get $list; markdown2html add to $out"
 done
+echo '</td><td style="vertical-align:top; text-align:top;"><!--TableOfContents:Begin-->There is TableOfContents<!--TableOfContents:End--></td>' >> $out;
 echo '</body></html>' >> $out;
 
+
 #add by gdbdzgd insert image and put image in the center for zh only
+echo  -n "Inserting Image ."
 grep  "Insert [0-9]*[a-z]*[0-9]*" -o  $out |while read f
 do
-	 sed -i  "s/${f}.png/<img src=\"figures\/${f##Insert }-tn.png\" \/>/g" $out
+	echo -n "."
+	sed -i  "s/${f}.png/<img src=\"figures\/${f##Insert }-tn.png\" \/>/g" $out
 done
 
-sed -i  "/图 [0-9]-[0-9]\./a </div>" $out
+echo ""
+echo "adjust Image in the middle"
 sed -i  "/[0-9]*[a-z]*[0-9]*-tn.png/i <div align=\"center\"> " $out
+sed -i  "/^图 [0-9]*-[0-9]*\.\|^Figure [0-9]*-[0-9]*\./a </div>" $out
+
+echo "make html Toc Contens..."
+perl htmlscripts/HtmlHeadingsToTableOfContents.pl $out 2>/dev/null
+
+echo 'add button "返回顶部"'
+sed -i '/<\/head>/i<style type="text/css">\n\t#floater{\n\t\tword-break:break-all;\n\t\tposition:fixed;\n\t\tfont-size:20%;\n\t\t_position:absolute;\n\t\tright:20px;\n\t\tbottom:20px;\n\t\tbackground:white;\n\t}\n </style>' $out 
+sed -i '/<body>/a<div>\n <a name="top"> \n<h1 id="floater"><a href="#header">^返回顶部<\/a><\/h1>\n <\/div>\n <A NAME="header"><\/A>\n' $out
